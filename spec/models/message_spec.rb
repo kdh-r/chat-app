89c78c285965cbfb191c0e1df351a41642a417f4
kdh-r <kdh.r1008@gmail.com>
2020-11-13 00:02:05 +0900

    Update message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -26,6 +26,20 @@ RSpec.describe Message, type: :model do
       @message.image = nil
       @message.valid?
       expect(@message.errors.full_messages).to include("Content can't be blank")
+
+      it 'roomが紐付いていないと保存できないこと' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Room must exist")
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("User must exist")
+      end
+
+
     end
   end
 end
